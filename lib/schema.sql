4512d4df4df9bfce64fdd70ecb02ee685c9b3424
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-27 18:38:50 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -8,4 +8,4 @@ CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT);
 
 CREATE TABLE characters (id INTEGER PRIMARY KEY, motto TEXT, species TEXT, author_id INTEGER);
 
-CREATE TABLE character_books (id INTEGER PRIMARY KEY, character_id INTEGER, book_id INTEGER);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);
