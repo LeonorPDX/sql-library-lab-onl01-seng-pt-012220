13abfec1f958a0dcac0561b3a782d337d65f6a26
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-27 18:38:24 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,3 +1,11 @@
 CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
 
-CREATE TABLE subgenres
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
+
+CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
+
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT);
+
+CREATE TABLE characters (id INTEGER PRIMARY KEY, motto TEXT, species TEXT, author_id INTEGER);
+
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, character_id INTEGER, book_id INTEGER);
